4a4519bd2429a559b709d54d02316bf57de51cf2
Truong Van Minh <vanminhtruong95@gmail.com>
2024-10-07 16:39:23 +0700

    session-4

---
+++ index.js
@@ -0,0 +1,10 @@
+let header_menu = document.querySelector("header .menu");
+
+function openSidebar() {
+    header_menu.style.width = "300px";
+}
+
+
+document.addEventListener("click", openSidebar);
+
+
